1d8f29db1e83587a2e989de81666f617c39fed4c
Jason Nordheim <jason.nordheim@gmail.com>
2020-03-13 20:36:31 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -39,7 +39,7 @@ def game_hash
           :slam_dunks => 15,
         },
         {
-          :player_name => "Mason Plumee",
+          :player_name => "Mason Plumlee",
           :number => 1,
           :shoe => 19,
           :points => 26,
@@ -130,18 +130,20 @@ end
 def num_points_scored(player_name)
   gh = game_hash
 
-  # check home team
+  # check home team 
   home_players = gh[:home][:players]
   home_player = home_players.find { |x| x[:player_name] == player_name }
-  # if home_player is not null, we have found the player
-  if home_player
+  # if home_player is not null, we have found the player 
+  if home_player 
     return home_player[:points]
-  end
+  end 
 
-  # was not a home team player, find in the away team
+  # was not a home team player, find in the away team 
   away_players = gh[:away][:players]
   away_player = away_players.find { | x | x[:player_name] == player_name }
   if away_player
     return away_player[:points]
   end
 end
+
+p num_points_scored('DeSagna Diop')
